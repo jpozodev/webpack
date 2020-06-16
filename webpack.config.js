7261e73eaaf2094ad00a4ddf1246f1526ce0d538
Jesús Pozo Rojas <jpozo@plainconcepts.com>
2020-06-16 20:17:25 +0200

    advanced config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    context: path.resolve(__dirname, "src"),
     mode: 'development',
-    entry: './src/app.js',
+    entry: './app.js',
     output: {
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist")
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader']
             }
-        ],
+        ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Mi primera app generada con webpack',
+            filename: 'index2.html',
+            template: 'template.html',
+            hash: true
+          })
+    ]
 };
