1b4e92be41472722d664def14620f6ac3f2d5757
Jesús Pozo Rojas <jpozo@plainconcepts.com>
2020-06-18 10:39:06 +0200

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
         filename: '[name]-bundle.js',
         path: path.resolve(__dirname, "dist")
     },
+    devServer: {
+        contentBase: './dist',
+        watchContentBase: true
+    },
     module: {
         rules: [
             {
